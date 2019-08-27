c7918dfc4e2af79012e4fc9c62c2c2f54cbf5af5
Parthasarathi <54533839+paarthasarathi@users.noreply.github.com>
2019-08-27 16:05:06 +0530

    Add files via upload

---
+++ bal part 2.py
@@ -0,0 +1,8 @@
+for i in range(15):
+    if i>10:
+        print('enough balance')
+    elif i==10:
+        print('balance getting low rechage soon')
+    else:
+        print('recharge')
+        
